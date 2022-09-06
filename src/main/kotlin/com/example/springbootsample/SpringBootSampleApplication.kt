f88cb86096518997e1b4f873292b159e9772f912
Veget-ables <makuhari109@gmail.com>
2022-09-06 21:37:21 +0900

    first commit

---
+++ src/main/kotlin/com/example/springbootsample/SpringBootSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootSampleApplication>(*args)
+}
